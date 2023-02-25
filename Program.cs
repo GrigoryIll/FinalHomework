db0e788585556606f05e6b702f04c0b38b82f821
Grigory <grigory@i.ru>
2023-02-25 18:17:40 +0300

    создан файл Program.cs с решением задачи

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿void StringValues(string[] args)
+{
+    string[] values = {"hello", "2", "world", ":=)"};
+    for (int i = 0; i < values.Length; i++)
+    {
+        if (values[i].Length <= 3)
+        {
+            Console.Write(values[i] + " ");
+        }
+    }
+}
+StringValues(args);
